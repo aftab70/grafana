19156466c236836a1b51552b4983f251fd9017f4
aftab70 <43289912+aftab70@users.noreply.github.com>
2020-09-08 12:49:33 +0530

    Update Grafana_installation.sh

--- Grafana_installation.sh
+++ Grafana_installation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 apt-get update
+cd /tmp
 wget https://dl.grafana.com/oss/release/grafana_5.4.3_amd64.deb
 apt-get install -y adduser libfontconfig
 dpkg -i grafana_5.4.3_amd64.deb 
 systemctl daemon-reload
 service grafana-server start
-service grafana-server status
 wget https://s3-eu-west-1.amazonaws.com/deb.robustperception.io/41EFC99D.gpg | apt-key add -
 apt-get update
 apt-get install prometheus prometheus-node-exporter prometheus-pushgateway prometheus-alertmanager -y
